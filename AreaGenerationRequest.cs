66b9bfb558153d172865a5116a9795237a3cf008
loudent2 <trregister@chunkychode.com>
2025-06-17 21:39:46 -0700

    Base plugin stuff. Nothing interesting yet

---
+++ AreaGenerationRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AIAreaGeneratorPlugin
+{
+    public enum AreaType
+    {
+        Outdoor,
+        Indoor
+    }
+
+    public class AreaGenerationRequest
+    {
+        public string AreaName { get; set; } = string.Empty;
+        public AreaType Type { get; set; }
+        public int Width { get; set; } = 16;
+        public int Height { get; set; } = 16;
+        public string Tileset { get; set; } = string.Empty;
+        public string Theme { get; set; } = string.Empty;
+
+        public override string ToString()
+        {
+            return $"{Type} Area '{AreaName}' ({Width}x{Height}) using '{Tileset}', theme: '{Theme}'";
+        }
+    }
+}
---
+++ AreaGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace AIAreaGeneratorPlugin
+{
+    public static class AreaGenerator
+    {
+        public static void Generate(AreaGenerationRequest request)
+        {
+            // For now, just log what would happen
+            string summary = $"[GENERATOR] Creating {request.Type} area:\n" +
+                             $"- Name: {request.AreaName}\n" +
+                             $"- Size: {request.Width} x {request.Height}\n" +
+                             $"- Tileset: {request.Tileset}\n" +
+                             $"- Theme: {request.Theme}";
+
+            MessageBox.Show(summary, "AreaGenerator Called");
+
+            // TODO: Load a template .ARE/.GIT or build from scratch
+            // TODO: Generate tile/terrain layout
+            // TODO: Write the resulting area files
+        }
+    }
+}
